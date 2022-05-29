d536334fc6e059161767ebc772f2db76dbc7e4bd
Rafael Lefever <rafael.lefever@discomco.pl>
2022-05-29 19:17:46 +0200

    adding kafka-kraft

--- 01-K3S/beam00.lab/setup-master-node00.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-curl -sfL https://get.k3s.io | sh -s - server   \
-  --datastore-endpoint="mysql://user:pass@tcp(ip_address:3306)/databasename" \
-  --disable traefik \
-  --node-taint CriticalAddonsOnly=true:NoExecute \
-  --tls-san 192.168.1.2 \
-  --tls-san k3s.home.lab
